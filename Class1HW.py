aae7d725a80b97d56e6f18c91e90cc725166865a
stephanie pi <stephmpi@gmail.com>
2014-01-16 19:40:56 -0800

    Added more possible answers

--- Class1HW.py
+++ Class1HW.py
@@ -2,18 +2,16 @@ print('Popsicle Stick: Knock knock')
 input_value = raw_input("Me:")
 
 
-if input_value == "Who's there?": 
+if input_value == "Who's there?" or input_value == "Who is there?" or input_value == "who's there?" or input_value == "who is there?": 
 	print ("Popsicle Stick: Orange")
 	input_value2 = raw_input ("Me:")
 	
-	if input_value2 == "Orange who?": 
+	if input_value2 == "Orange who?" or "orange who?": 
 		print ("Popsicle Stick: Orange you gonna let me in??")
 	else:
 		print ("Try again..!")
 else:
 	print ("Try again!")
-
-
 
 
 
