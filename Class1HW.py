a0ccb223f773975e886707076191c1ec4f15f05e
stephanie pi <stephmpi@gmail.com>
2014-01-16 19:10:29 -0800

    Homework Assignment

---
+++ Class1HW.py
@@ -0,0 +1,19 @@
+print('Popsicle Stick: Knock knock')
+input_value = raw_input("Me:")
+
+
+if input_value == "Who's there?": 
+	print ("Popsicle Stick: Orange")
+	input_value2 = raw_input ("Me:")
+	
+	if input_value2 == "Orange who?": 
+		print ("Popsicle Stick: Orange you gonna let me in??")
+	else:
+		print ("Try again..!")
+else:
+	print ("Try again!")
+
+
+
+
+
