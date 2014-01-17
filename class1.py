793fd1ac0d7f894f16f81b20d20c45f45453fce7
stephanie pi <stephmpi@gmail.com>
2014-01-16 17:45:51 -0800

    Intro to python class code

---
+++ class1.py
@@ -0,0 +1,6 @@
+print('I am a python program.')
+input_value = raw_input("Enter a radius:")
+radius = float(input_value)
+area = 3.14159 * radius ** 2
+print ("The area of a circle with radius " + input_value + " is:")
+print ('area')
