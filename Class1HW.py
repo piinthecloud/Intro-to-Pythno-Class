4fad2e997fa94e468dbb522c6d2e20431c32d648
stephanie pi <stephmpi@gmail.com>
2014-01-18 09:52:47 -0800

    Simplified possible answers by added .lower() and making all input lower case. this way, it only has to check against lower case answers.

--- Class1HW.py
+++ Class1HW.py
@@ -1,17 +1,16 @@
 print('Popsicle Stick: Knock knock')
-input_value = raw_input("Me:")
+input_value = raw_input("Me:").lower()
 
-
-if input_value == "Who's there?" or input_value == "Who is there?" or input_value == "who's there?" or input_value == "who is there?": 
+if input_value == "who's there?" or input_value == "who is there?": 
 	print ("Popsicle Stick: Orange")
+	
 	input_value2 = raw_input ("Me:")
 	
-	if input_value2 == "Orange who?" or "orange who?": 
+	if input_value2 == "Orange who?" or input_value2 == "orange who?": 
 		print ("Popsicle Stick: Orange you gonna let me in??")
 	else:
 		print ("Try again..!")
+
 else:
 	print ("Try again!")
-
-
-
+  
